4b37b5078665862435289de9b7c88349ba682862
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2022-12-26 21:28:20 +0500

    send back message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/login")]
+fn login() -> &'static str {
+    "Login screen"
+}
+
+#[get("/hi/<name>")]
+fn hi(name: &str) -> String {
+    format!("Hello how are you {}", name)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/auth", routes![login])
+        .mount("/in", routes![hi])
 }
